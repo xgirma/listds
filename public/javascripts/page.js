994613512335c09e5be176d4d490299d908e796d
Girma Nigusse <xgirma@gmail.com>
2016-05-15 23:17:41 -0400

    add jquery

--- public/javascripts/page.js
+++
@@ -1,5 +0,0 @@
-function startPage() {
-  document.getElementById('close-kiosk').disabled = true;
-  document.getElementById('john-end').disabled = true;
-  document.getElementById('jane-end').disabled = true;
-}
