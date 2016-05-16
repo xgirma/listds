ce67a41dc0174de8eb66713b6688d7a90bce961b
Girma Nigusse <xgirma@gmail.com>
2016-05-15 22:59:54 -0400

    add movie list css

---
+++ public/javascripts/page.js
@@ -0,0 +1,5 @@
+function startPage() {
+  document.getElementById('close-kiosk').disabled = true;
+  document.getElementById('john-end').disabled = true;
+  document.getElementById('jane-end').disabled = true;
+}
