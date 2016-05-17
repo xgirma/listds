76f02232f1a514c6a7738658aff85cdc9108f914
Girma Nigusse <xgirma@gmail.com>
2016-05-16 20:49:53 -0400

    remove movies

--- public/javascripts/kiosk.js
+++ public/javascripts/kiosk.js
@@ -12,67 +12,92 @@ var btnOpen = document.querySelector('#open')
   , lblJaMsg = document.querySelector('#jane-msg')
 
   , closedMsg = 'Kiosk is closed'
-  , rentMsg = 'choose movie on the left and click rent. click end when finished';
+  , rentMsg = 'choose movie on the left and click rent. click end when finished'
 
-$(document).ready(function() {
+  , collection = [
+            "The Shawshank Redemption",
+            "The Godfather",
+            "The Godfather: Part II",
+            "Pulp Fiction",
+            "The Good, the Bad and the Ugly",
+            "12 Angry Men",
+            "Schindler’s List",
+            "The Dark Knight",
+            "The Lord of the Rings: The Return of the King",
+            "Fight Club",
+            "Star Wars: Episode V - The Empire Strikes Back",
+            "One Flew Over the Cuckoo’s Nest",
+            "The Lord of the Rings: The Fellowship of the Ring",
+            "Inception",
+            "Goodfellas",
+            "Star Wars",
+            "Seven Samurai",
+            "The Matrix",
+            "Forrest Gump",
+            "City of God"];
+
+$(document).ready(function () {
   $(btnOpen).prop('disabled', false);
   $(btnClose).prop('disabled', true);
   init();
 });
 
 // kiosk
-$(btnOpen).click(function(){
+$(btnOpen).click(function () {
   $(btnClose).prop('disabled', false);
   $(btnOpen).prop('disabled', true);
   $(lblJoMsg).html('');
   $(lblJaMsg).html('');
 });
 
-$(btnClose).click(function(){
+$(btnClose).click(function () {
   init();
   $(btnOpen).prop('disabled', false);
   $(btnClose).prop('disabled', true);
 });
 
 // john
-$(btnJoStart).click(function(){
-  if($(btnOpen).is(':disabled')){
+$(btnJoStart).click(function () {
+  if ($(btnOpen).is(':disabled')) {
     $(btnJoStart).prop('disabled', true);
     $(btnJaStart).prop('disabled', true);
     $(btnJoRent).prop('disabled', false);
     $(btnJoEnd).prop('disabled', false);
     $(lblJoMsg).html(rentMsg);
-  } else{
+  } else {
     $(lblJoMsg).html(closedMsg);
   }
 });
 
-$(btnJoRent).click(function(){});
+$(btnJoRent).click(function () {
 
-$(btnJoEnd).click(function(){
+});
+
+$(btnJoEnd).click(function () {
   init()
 });
 
 // jane
-$(btnJaStart).click(function(){
-  if($(btnOpen).is(':disabled')){
+$(btnJaStart).click(function () {
+  if ($(btnOpen).is(':disabled')) {
     $(btnJaStart).prop('disabled', true);
     $(btnJoStart).prop('disabled', true);
     $(btnJaRent).prop('disabled', false);
     $(btnJaEnd).prop('disabled', false);
     $(lblJaMsg).html(rentMsg);
-  } else{
+  } else {
     $(lblJaMsg).html(closedMsg);
   }
 });
 
-$(btnJaRent).click(function(){});
+$(btnJaRent).click(function () {
+});
 
-$(btnJaEnd).click(function(){
+$(btnJaEnd).click(function () {
   init();
 });
 
-function init(){
+function init() {
   $(btnJoStart).prop('disabled', false);
   $(btnJoRent).prop('disabled', true);
   $(btnJoEnd).prop('disabled', true);
